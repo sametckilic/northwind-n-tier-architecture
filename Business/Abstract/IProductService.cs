a8b8a4b85c91ad329c2e2ea23f30ce56067a0bd1
samet <205541038@firat.edu.tr>
2023-08-23 03:49:54 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+    }
+}
---
+++ Bussinies/Concrete/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _ProductDal;
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
